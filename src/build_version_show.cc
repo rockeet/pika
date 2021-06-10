4af0016e3d35e875e4cfb2ad196559b90ad7e038
gukaifeng <github@gukaifeng.me>
2021-06-10 15:19:53 +0800

    increase date_time_chs[] size

--- src/build_version_show.cc
+++ src/build_version_show.cc
@@ -23,11 +23,11 @@ public:
 
     json js;
 
-    char date_time_chs[20];
+    char date_time_chs[22];
     tm date_time_tm;
     sprintf(date_time_chs, "%s %s", pika_build_compile_date, pika_build_compile_time);
     strptime(date_time_chs, "%b %d %Y %T", &date_time_tm);
-    strftime(date_time_chs, 20, "%Y-%m-%d %T", &date_time_tm);
+    strftime(date_time_chs, 22, "%Y-%m-%d %T", &date_time_tm);
     js["build_compile_datetime"] = date_time_chs;
 
     js["build_git_sha"] = json::object({
