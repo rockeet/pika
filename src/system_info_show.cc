b389cf1c5d958f9175114c7fac9d893292ebc347
gukaifeng <github@gukaifeng.me>
2021-08-04 00:35:07 +0800

    updated system_info_show.cc

--- src/system_info_show.cc
+++ src/system_info_show.cc
@@ -5,7 +5,6 @@
 #include <terark/io/StreamBuffer.hpp>
 #include <terark/util/process.hpp>
 #include <terark/num_to_str.hpp>
-#include <iostream>
 
 namespace topling {
 using namespace ROCKSDB_NAMESPACE;
@@ -20,99 +19,78 @@ public:
   std::string ToString(const json &dump_options, const SidePluginRepo &) const {
     json js;
 
-    ProcPipeStream m_file;
-    LittleEndianDataInput<InputBuffer> m_reader;
+    ProcPipeStream file;
+    LittleEndianDataInput<InputBuffer> reader;
 
-    const size_t buf_size = 2048;
-    m_reader.set_bufsize(buf_size);
+    const size_t buf_size = 4096;
+    reader.set_bufsize(buf_size);
     char buf[buf_size];
+
+    // 读取并处理内存信息
+    file.open("free", "r");
+    reader.attach(&file);
+    reader.read(buf, buf_size);
+    file.close();
+
     std::stringstream ss;
-
-    m_file.open("free", "r");
-    m_reader.attach(&m_file);
-    m_reader.read(buf, buf_size);
-    m_file.close();
-
     ss.str(buf);
     std::string mem[17];
     for (size_t i = 0; i < 17; ++i)
-      ss >> mem[i];   
+      ss >> mem[i];
 
-    m_file.open("env LC_ALL=C lscpu", "r");
-    m_reader.attach(&m_file);
-    m_reader.read(buf, buf_size);
-    m_file.close();
-
-    ss.str(buf);
-    std::string cpu_info;
-    std::map<std::string, std::string> cpu_map;
-    for(size_t i = 0; i < 26 && getline(ss, cpu_info); ++i) {
-      size_t colon_pos = cpu_info.find(':');
-      cpu_map[cpu_info.substr(0, colon_pos)] = cpu_info.substr(colon_pos + 1);
-    }
-
-    js["CPU info"]["CPU basic info"]["Architecture"] = cpu_map["Architecture"];
-    js["CPU info"]["CPU basic info"]["CPU(s)"] = cpu_map["CPU(s)"];
-    js["CPU info"]["CPU basic info"]["Thread(s) per core"] = cpu_map["Thread(s) per core"];
-    js["CPU info"]["CPU basic info"]["Core(s) per socket"] = cpu_map["Core(s) per socket"];
-    js["CPU info"]["CPU basic info"]["Socket(s)"] = cpu_map["Socket(s)"];
-    js["CPU info"]["CPU basic info"]["Model name"] = cpu_map["Model name"];
-    js["CPU info"]["CPU basic info"]["Virtualization"] = cpu_map["Virtualization"];
-
-    js["CPU info"]["NUMA node(s)"]["total"] = cpu_map["NUMA node(s)"];
-    js["CPU info"]["NUMA node(s)"]["node0"] = cpu_map["NUMA node0 CPU(s)"];
-    js["CPU info"]["NUMA node(s)"]["node1"] = cpu_map["NUMA node1 CPU(s)"];
-  
-    js["CPU info"]["CPU Clock Speed"]["MHz"] = cpu_map["CPU MHz"];
-    js["CPU info"]["CPU Clock Speed"]["max MHz"] = cpu_map["CPU max MHz"];
-    js["CPU info"]["CPU Clock Speed"]["min MHz"] = cpu_map["CPU min MHz"];
-
-    js["CPU info"]["cache"]["L1d"] = cpu_map["L1d cache"];
-    js["CPU info"]["cache"]["L1i"] = cpu_map["L1i cache"];
-    js["CPU info"]["cache"]["L2"] = cpu_map["L2 cache"];
-    js["CPU info"]["cache"]["L3"] = cpu_map["L3 cache"];
-
-    js["CPU info"]["Flags"] = cpu_map["Flags"];
-
-    // if (dump_options["html"] == "0") {
-    //   for (size_t i = 0; i < 6; ++i)
-    //     js["Memory info"]["Mem"][mem[i]] = mem[i + 7];
-    //   for (size_t i = 0; i < 3; ++i)
-    //     js["Memory info"]["Swap"][mem[i]] = mem[i + 14];
-    //   return JsonToString(js, dump_options);
-    // }
-
-    // if (dump_options["html"] == "1")
-    json lmem, jmem;
-    jmem[" "] = "Mem";
-    jmem["total"] = mem[7];
-    jmem["used"] = mem[8];
-    jmem["free"] = mem[9];
-    jmem["shared"] = mem[10];
-    jmem["buff/cache"] = mem[11];
-    jmem["available"] = mem[12];
+    json lmem, jmem, jswap;
+    jmem = json::object({
+      {"", "Mem"},
+      {"total", mem[7]},
+      {"used", mem[8]},
+      {"free", mem[9]},
+      {"shared", mem[10]},
+      {"buff/cache", mem[11]},
+      {"available", mem[12]},
+    });
+    jswap = json::object({
+      {"", "Swap"},
+      {"total", mem[14]},
+      {"used", mem[15]},
+      {"free", mem[16]},
+      {"shared", ""},
+      {"buff/cache", ""},
+      {"available", ""},
+    });
     lmem.push_back(std::move(jmem));
-
-    jmem[" "] = "Swap";
-    jmem["total"] = mem[14];
-    jmem["used"] = mem[15];
-    jmem["free"] = mem[16];
-    jmem["shared"] = "";
-    jmem["buff/cache"] = "";
-    jmem["available"] = "";
-    lmem.push_back(std::move(jmem));
+    lmem.push_back(std::move(jswap));
 
     auto &cols = lmem[0]["<htmltab:col>"];
-    cols = json::array({
-        " ",
-        "total",
-        "used",
-        "free",
-        "shared",
-        "buff/cache",
-        "available",
-    });
+    cols = json::array({"", "total", "used", "free", "shared", "buff/cache", "available",});
     js["Memory Info"] = std::move(lmem);
+
+    // 读取并处理 CPU 信息
+    file.open("env LC_ALL=C lscpu -J", "r");
+    reader.attach(&file);
+    reader.read(buf, buf_size);
+    file.close();
+
+    json temp_js = json::parse(buf);
+    std::set<std::string> cpu_basic{"Architecture", "CPU(s)", "Thread(s) per core", "Core(s) per socket", "Socket(s)", "Model name", "Virtualization"};
+    std::set<std::string> cpu_numa{"NUMA node(s)", "NUMA node0 CPU(s)", "NUMA node1 CPU(s)"};
+    std::set<std::string> cpu_clockspeed{"CPU MHz", "CPU max MHz", "CPU min MHz"};
+    std::set<std::string> cpu_cache{"L1d cache", "L1i cache", "L2 cache", "L3 cache"};
+    std::set<std::string> cpu_flags{"Flags"};
+    for (auto kv: temp_js["lscpu"]) {
+      std::string k = kv["field"];
+      k.pop_back();
+      auto& js_cpu = js["CPU Info"];
+      if (cpu_basic.count(k))
+        js_cpu["Basic"][k] = kv["data"];
+      else if (cpu_numa.count(k))
+        js_cpu["NUMA"][k] = kv["data"];
+      else if (cpu_clockspeed.count(k))
+        js_cpu["Clock Speed"][k] = kv["data"];
+      else if (cpu_cache.count(k))
+        js_cpu["Cache"][k] = kv["data"];
+      else if (cpu_flags.count(k))
+        js_cpu[k] = kv["data"];        
+    }
 
     return JsonToString(js, dump_options);
   }
