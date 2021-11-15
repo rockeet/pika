e6ed48087eeab38662d6296c069753d2cfd5fa89
leipeng <rockeet@gmail.com>
2021-11-15 13:50:12 +0800

    topling-build.sh: del old useless code

--- topling-build.sh
+++ topling-build.sh
@@ -6,7 +6,6 @@ fi
 
 set -e
 
-ROCKSDB_DIR=${ROCKSDB_DIR:-"../rocksdb"}
 TOPLING_CORE_DIR=${TOPLING_CORE_DIR:-"../topling-core"}
 WITH_BMI2=`bash ${TOPLING_CORE_DIR}/cpu_has_bmi2.sh`
 tmpfile=`mktemp -u compiler-XXXXXX`
@@ -16,22 +15,10 @@ UNAME_MachineSystem=`uname -m -s | sed 's:[ /]:-:g'`
 BUILD_NAME=${UNAME_MachineSystem}-${COMPILER}-bmi2-${WITH_BMI2}
 #BUILD_ROOT=build/${BUILD_NAME}
 
-LIB_SUFFIX_ARRAY=(r a d) # rls afr dbg
-LIB_SUFFIX=`eval 'echo ${LIB_SUFFIX_ARRAY['$DEBUG_LEVEL']}'`
 export LD_LIBRARY_PATH=/node-shared/lib:${LD_LIBRARY_PATH}
 
 env PATH=/node-shared/vcpkg/installed/x64-linux/tools/protobuf:$PATH \
-    CXXFLAGS="-DJSON_USE_GOLD_HASH_MAP=1 \
-              -DROCKSDB_PLATFORM_POSIX=1 \
-              -DFOLLY_NO_CONFIG=1 \
-              -I${PWD}/third/folly \
-              -I/node-shared/vcpkg/installed/x64-linux/include \
-              -I/node-shared/leipeng/osc/rocksdb/sideplugin/rockside/src \
-              -I/node-shared/leipeng/osc/rocksdb/sideplugin/topling-core/boost-include \
-              -I/node-shared/leipeng/osc/rocksdb/sideplugin/topling-core/3rdparty/zstd \
-              -I/node-shared/leipeng/osc/rocksdb/sideplugin/topling-rocks/src \
-              -I/node-shared/leipeng/osc/topling-core/src" \
-    LDFLAGS="-L/node-shared/lib -lterark-{zbs,core}-${COMPILER}-${LIB_SUFFIX}" \
+    CXXFLAGS="-I/node-shared/vcpkg/installed/x64-linux/include" \
     PROTO_BUF_LDFLAGS="-L/node-shared/lib -letcd-cpp-api" \
     ROCKSDB_PATH="/node-shared/leipeng/osc/rocksdb" \
     make DISABLE_UPDATE_SB=1 DISABLE_WARNING_AS_ERROR=1 $@
